fb222a9f38bde956d81c5b4658f39b17c104beea
Sajedullah Aref <152142322+rf104@users.noreply.github.com>
2024-09-12 17:34:43 +0600

    Create Float_or_int.cpp

---
+++ Float_or_int.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s;
+    cin>>s;
+    int l = s.length();
+    int flg = 0,ans = 0;
+    string pt = "";
+    for(int i=0;i<l;i++)
+    {
+        if(s[i]=='.') flg = 1;
+        else if(flg==1&&s[i]!='0'){
+            ans  = 1;
+            pt = pt + s[i];
+        }
+        else if(flg==1){
+            pt = pt + s[i];
+        }
+    }
+    if(ans==1) cout<<"float "<<stoi(s)<<" 0."<<pt<<endl;
+    else cout<<"int "<<stoi(s)<<endl;
+}
